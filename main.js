18681c9447deef29cf5aef1f4d30d53b6537e87e
rafiguadalupe <83619619+rafiguadalupe@users.noreply.github.com>
2021-12-04 17:43:20 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -20,4 +20,19 @@ function modelLoaded()
 console.log("model loaded")
 }
 
-
+function check(){
+    img=document.getElementById('captured_image');
+    classifier.classify(img,gotResult);
+    
+}
+function gotResult(error,results)
+{
+if(errror){
+    console.error(error);                                 
+}
+else{
+    console.log(results);
+    document.getElementById("result_object_name").innerHTML=results[0].label;
+    document.getElementById("result_object_accuracy").innerHTML=results[0].confidence.toFixed(4);
+}
+}
